985b282f4913a72426da28c482fc49121a20ac5e
JASMANY <jsanchezm2@unemi.edu.ec>
2025-04-05 19:34:56 -0500

    WEB SOCKETS

--- fastchatdj/asgi.py
+++ fastchatdj/asgi.py
@@ -1,19 +1,15 @@
-"""
-ASGI config for fastchatdj project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-import whatsapp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fastchatdj.settings')
+django.setup()
+
+# Importar después de configurar Django
+import whatsapp.routing  # Ajusta según el nombre de tu app
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
