ee34cb716b679ebd6a5826e6853b320d353fb5f7
jguachuns <jguachuns@unemi.edu.ec>
2025-04-14 17:17:29 -0500

    feat: Update FormParent initialization to safely handle instance parameter

--- core/custom_models.py
+++ core/custom_models.py
@@ -192,7 +192,7 @@ class FormParent:
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
         self.ver = kwargs.pop('ver', False)
-        self.instance = kwargs.pop('instance', None)
+        # self.instance = kwargs.pop('instance', None)
         customStep = kwargs.pop('customStep', None)
         cantCharsSelect2 = kwargs.pop('cantCharsSelect2', {})
         addDataNameInput = kwargs.pop('addDataNameInput', False)
@@ -205,14 +205,14 @@ class FormParent:
         no_select2 = kwargs.pop('no_select2', [])#listado d campos que no quieran q se renderice con la librería select2
         inlines = kwargs.pop('inlines', [])
         # Solo extrae el booleano de edición aquí, sin tocar `self.instance` aún
-        self.editando = bool(self.instance)
+        self.editando = bool('instance' in kwargs and kwargs.get("instance") and kwargs.get("instance").id)
 
         # Llama al constructor padre PRIMERO
         super(FormParent, self).__init__(*args, **kwargs)
 
         # AHORA puedes usar self.instance de forma segura
         if self.editando:
-            self.instancia = self.instance  # Aquí ya existe `self.instance`
+            self.instancia = kwargs['instance']
         super(FormParent, self).__init__(*args, **kwargs)
 
         if inlines:
