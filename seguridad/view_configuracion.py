78d7f16e1cf64b09c91c23c11a1f16b067eb9ef2
hllerenaa <hllerenaa1h@gmail.com>
2025-04-22 21:59:50 -0500

    LOG AUTOMATICO USERREGISTRO USERCREWACION

--- seguridad/view_configuracion.py
+++ seguridad/view_configuracion.py
@@ -8,7 +8,8 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from core.custom_models import FormError
-from core.funciones import addData, salva_auditoria, secure_module, log
+from core.funciones import addData, salva_auditoria, secure_module, log, remover_caracteres_especiales_unicode, \
+    generar_nombre
 from core.funciones_adicionales import salva_logs
 from seguridad.forms import ConfiguracionForm
 from seguridad.models import Configuracion
@@ -44,22 +45,19 @@ def configuracion(request):
                         log(f"Elimino icono {c.nombre_empresa}", request, "del")
 
                         return JsonResponse({'state': True})
-
-                    if action == 'eliminar_logo':
+                    elif action == 'eliminar_logo':
                         c = model.objects.get(pk=int(request.POST["pk"]))
                         c.logo_sistema = ''
                         c.save(request)
                         log(f"Elimino logo {c.nombre_empresa}", request, "del")
                         return JsonResponse({'state': True})
-
-                    if action == 'eliminar_logo_white':
+                    elif action == 'eliminar_logo_white':
 
                         c = model.objects.get(pk=int(request.POST["pk"]))
                         c.logo_sistema_white = ''
                         c.save(request)
                         log(f"Elimino logo {c.nombre_empresa}", request, "del")
                         return JsonResponse({'state': True})
-
                     elif action == 'eliminar_fondo_perfil':
 
                         c = model.objects.get(pk=int(request.POST["pk"]))
@@ -67,7 +65,6 @@ def configuracion(request):
                         c.save(request)
                         log(f"Elimino fondo perfil {c.nombre_empresa}", request, "del")
                         return JsonResponse({'state': True})
-
                     elif action == 'eliminar_banner_login':
 
                         c = model.objects.get(pk=int(request.POST["pk"]))
@@ -75,16 +72,51 @@ def configuracion(request):
                         c.save(request)
                         log(f"Elimino banner login {c.nombre_empresa}", request, "del")
                         return JsonResponse({'state': True})
-
                     elif action == 'change':
 
                         form = Formulario(request.POST, request.FILES, instance=confi, request=request)
                         if form.is_valid():
-                        # if form.is_valid():
-                            obj = form.save()
-                            if request.FILES.get('archivo_manual'):
-                                obj.fecha_reg_manual = datetime.now()
-                                obj.save()
+                            if 'ico' in request.FILES:
+                                file = request.FILES['ico']
+                                nombredocumento = remover_caracteres_especiales_unicode('ico')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.ico = file
+                            if 'logo_sistema' in request.FILES:
+                                file = request.FILES['logo_sistema']
+                                nombredocumento = remover_caracteres_especiales_unicode('logo_sistema')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.logo_sistema = file
+                            if 'logo_sistema_white' in request.FILES:
+                                file = request.FILES['logo_sistema_white']
+                                nombredocumento = remover_caracteres_especiales_unicode('logo_sistema_white')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.logo_sistema_white = file
+                            if 'fondo_perfil' in request.FILES:
+                                file = request.FILES['fondo_perfil']
+                                nombredocumento = remover_caracteres_especiales_unicode('fondo_perfil')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.fondo_perfil = file
+                            if 'banner_login' in request.FILES:
+                                file = request.FILES['banner_login']
+                                nombredocumento = remover_caracteres_especiales_unicode('banner_login')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.banner_login = file
+                            if 'fondoprincipal' in request.FILES:
+                                file = request.FILES['fondoprincipal']
+                                nombredocumento = remover_caracteres_especiales_unicode('fondoprincipal')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.fondoprincipal = file
+                            if 'imagenprincipal' in request.FILES:
+                                file = request.FILES['imagenprincipal']
+                                nombredocumento = remover_caracteres_especiales_unicode('imagenprincipal')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.imagenprincipal = file
+                            if 'imagen_landing' in request.FILES:
+                                file = request.FILES['imagen_landing']
+                                nombredocumento = remover_caracteres_especiales_unicode('imagen_landing')
+                                file._name = generar_nombre(nombredocumento, file._name)
+                                form.instance.imagen_landing = file
+                            form.save()
                             log(f"Edito Configuracion {form.instance.__str__()}", request, "change", obj=form.instance.id)
 
                             res_json.append({'error': False,
@@ -93,7 +125,6 @@ def configuracion(request):
                             messages.success(request, "Modificado correctamente.")
                         else:
                             raise FormError(form)
-
             except ValueError as ex:
                 res_json.append({'error': True,
                                  "message": str(ex)
@@ -113,3 +144,4 @@ def configuracion(request):
         data["form"] = Formulario(instance=confi)
         data["pk"] = confi.pk
         return render(request, 'seguridad/configuracion/form.html', data)
+    return None
