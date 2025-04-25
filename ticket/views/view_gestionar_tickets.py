e74ff46ff112512ac059430cbdcee4d684978fc8
jose <joseguachuns@hotmail.com>
2025-04-24 22:56:54 -0500

    feat: Add drag-and-drop functionality for ticket management and enhance ticket display

--- ticket/views/view_gestionar_tickets.py
+++ ticket/views/view_gestionar_tickets.py
@@ -1,4 +1,3 @@
-
 from datetime import date, datetime
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
@@ -10,7 +9,7 @@ from django.template.loader import get_template
 
 from autenticacion.models import Usuario
 from core.custom_models import FormError
-from core.funciones import addData, paginador,log
+from core.funciones import addData, paginador, log
 from django.contrib import messages
 
 from seguridad.models import Empresa
@@ -41,27 +40,76 @@ def ticketIntegranteView(request):
             with transaction.atomic():
 
                 if action == 'iniciarticket':
-                    pk=int(request.POST['id'])
+                    pk = int(request.POST['id'])
                     ticket = TicketAtencion.objects.get(id=pk)
-                    ticket.finicioactividad=datetime.now()
-                    ticket.estado=3
+                    ticket.finicioactividad = datetime.now()
+                    ticket.estado = 3
                     ticket.save(request)
                     messages.info(request, f'Se inicio correctamente el ticket: f{ticket.codigo}')
-                    log('Ticket eliminado correctamente', request,  ticket)
+                    log('Ticket eliminado correctamente', request, ticket)
                     return JsonResponse({'error': False})
 
                 elif action == 'finalizarticket':
-                    pk=int(request.POST['id'])
+                    pk = int(request.POST['id'])
                     ticket = TicketAtencion.objects.get(id=pk)
-                    ticket.ffinactividad=datetime.now()
-                    ticket.estado=4
+                    ticket.ffinactividad = datetime.now()
+                    ticket.estado = 4
                     ticket.save(request)
                     messages.success(request, f'Se finalizo correctamente el ticket: f{ticket.codigo}')
-                    log('Ticket eliminado correctamente', request,  ticket)
+                    log('Ticket eliminado correctamente', request, ticket)
                     return JsonResponse({'error': False})
 
+                if action == 'do':
+                    try:
+                        idactividad = request.POST['idactividad']
+                        actividad = TicketAtencion.objects.get(pk=idactividad)
+                        actividad.estado = 2
+                        actividad.ffinactividad = None
+                        actividad.save(request)
+                        log(u'Puso en pendiente una actividad: %s' % actividad, request, "edit")
+                        qsincidencia = TicketAtencion.objects.values('id').filter(status=True, asignadoa=request.user)
+                        res_json = {"resp": True, 'totdo': len(qsincidencia.filter(estado=2)),
+                                    'totprogress': len(qsincidencia.filter(estado=3)),
+                                    'totdone': len(qsincidencia.filter(estado=4)[:10])}
+                    except Exception as ex:
+                        res_json = {'resp': False, "message": "Error: {}".format(ex)}
+                    return JsonResponse(res_json, safe=False)
+
+                if action == 'progress':
+                    try:
+                        idactividad = request.POST['idactividad']
+                        actividad = TicketAtencion.objects.get(pk=idactividad)
+                        actividad.estado = 3
+                        actividad.ffinactividad = None
+                        actividad.save(request)
+                        log(u'Puso en marcha una incidencia: %s' % actividad, request, "change")
+                        qsincidencia = TicketAtencion.objects.values('id').filter(status=True, asignadoa=request.user)
+                        res_json = {"resp": True, 'totdo': len(qsincidencia.filter(estado=2)),
+                                    'totprogress': len(qsincidencia.filter(estado=3)),
+                                    'totdone': len(qsincidencia.filter(estado=4)[:10])}
+                    except Exception as ex:
+                        res_json = {'resp': False, "message": "Error: {}".format(ex)}
+                    return JsonResponse(res_json, safe=False)
+
+                if action == 'done':
+                    try:
+                        idactividad = request.POST['idactividad']
+                        actividad = TicketAtencion.objects.get(pk=idactividad)
+                        actividad.estado = 4
+                        actividad.ffinactividad = datetime.now()
+                        actividad.save(request)
+
+                        log(u'Finalizo un ticket: %s' % actividad, request, "edit")
+                        qsincidencia = TicketAtencion.objects.values('id').filter(status=True, asignadoa=request.user)
+                        res_json = {"resp": True, 'totdo': len(qsincidencia.filter(estado=2)),
+                                    'totprogress': len(qsincidencia.filter(estado=3)),
+                                    'totdone': len(qsincidencia.filter(estado=4)[:10])}
+                    except Exception as ex:
+                        res_json = {'resp': False, "message": "Error: {}".format(ex)}
+                    return JsonResponse(res_json, safe=False)
+
                 else:
-                    res_json.append({'error': True, "message": 'Acción no encontrada',})
+                    res_json.append({'error': True, "message": 'Acción no encontrada', })
         except ValueError as ex:
             res_json.append({'error': True,
                              "message": str(ex)
@@ -77,7 +125,8 @@ def ticketIntegranteView(request):
         if 'action' in request.GET:
             data["action"] = action = request.GET['action']
 
-        criterio, filtros, url_vars, documento = request.GET.get('criterio', ''), Q(status=True, asignadoa=request.user), '', request.GET.get('documento', '')
+        criterio, filtros, url_vars, documento = request.GET.get('criterio', ''), Q(status=True, asignadoa=request.user), '', request.GET.get(
+            'documento', '')
         estado = request.GET.get('estado', '')
 
         if estado:
@@ -119,10 +168,14 @@ def ticketIntegranteView(request):
                           Q(usuario__last_name__icontains=palabras[1]) &
                           Q(usuario__last_name__icontains=palabras[2]))
             palabras = criterio.strip()
-            filtros &= q_obj | Q(codigo__icontains=palabras) | Q(numero_ticket__icontains=palabras) | Q(titulo__icontains=palabras)
+            filtros &= q_obj | Q(codigo__icontains=palabras) | Q(numero_ticket__icontains=palabras) | Q(
+                titulo__icontains=palabras)
         listado = model.objects.filter(filtros).order_by('-codigo').distinct()
+        data['tickets_pendientes'] = listado.filter(estado=2)
+        data['tickets_enproceso'] = listado.filter(estado=3)
+        data['tickets_finalizadas'] = listado.filter(estado=4)
         data["url_vars"] = url_vars
-        data["list_count"] = listado.count()
+        data["list_count"] = len(listado)
         data['estados'] = model.ESTADO_TICKET
         paginador(request, listado, 20, data, url_vars)
         return render(request, 'ticket/view_mis_tickets.html', data)
