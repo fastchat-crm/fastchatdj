5aebdb07d5a279dc2fbf3ce0e59345735c5f52c7
JASMANY <jsanchezm2@unemi.edu.ec>
2025-06-23 02:54:59 -0500

    [FIX] - NO SE ESTABAN GUARDANDO LOS MENSAJES QUE SE ENVIABAN.

--- core/funciones.py
+++ core/funciones.py
@@ -855,10 +855,11 @@ def save_log_entry(mensaje, request, accion, user=None, obj=None):
         logaction = ADDITION
     else:
         logaction = CHANGE
-    content_type = obj and ContentType.objects.get_for_model(obj).id
+    content_type = obj and ContentType.objects.get_for_model(obj)
     LogEntry.objects.log_action(
-        user_id=request.user.id if not user else user.id, content_type_id=content_type and content_type.id,
-        object_id=obj and obj.id, object_repr=str(obj), action_flag=logaction, change_message=unicode(mensaje)
+        user_id=request and request.user and request.user.is_authenticated and request.user.id or user and user.id,
+        content_type_id=content_type and content_type.id, object_id=obj and obj.id, object_repr=str(obj),
+        action_flag=logaction, change_message=unicode(mensaje)
     )
 
 
