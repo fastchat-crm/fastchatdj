24e6afb972d5cca8ed2350975d1e929eeb561e03
hllerenaa <hllerenaa1h@gmail.com>
2026-04-17 10:07:43 -0500

    feat(api): implement multi-provider support for WhatsApp sessions and add Meta Cloud API configuration

--- public/urls.py
+++ public/urls.py
@@ -7,6 +7,7 @@ from .view_restaurar import restaurar
 from .view_login import login_tienda, logout_tienda
 from .view_recordarusername import recordarusername
 from .view_terminoscondiciones import terminosycondiciones
+from .view_quienessomos import quienessomos
 
 urlpatterns = [
     # path('', index),
@@ -19,5 +20,6 @@ urlpatterns = [
     re_path(r'^changepass/', changepass),
     path('privacidad/', terminosycondiciones),
     path('terminosycondiciones/', terminosycondiciones),
+    path('quienes-somos/', quienessomos),
 
 ]
